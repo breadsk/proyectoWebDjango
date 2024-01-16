61ca229c0eedd0e7bfbbf2f52a5385f53e01347a
zasory <nicolas.programador@gmail.com>
2024-01-16 11:09:14 -0300

    cambios recientes

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ from . import views
 from django.conf.urls.static import static
 
 urlpatterns = [    
-    path('', views.blog, name= "Blog")
+    path('', views.blog, name= "Blog"),
+    path('categoria/<int:categoria_id>/', views.categoria , name= "categoria")
 ]     
 
