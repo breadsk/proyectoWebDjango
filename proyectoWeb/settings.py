c3f481d1f413022f730a70dfcbddb7719265dba9
zasory <nicolas.programador@gmail.com>
2024-01-16 17:20:13 -0300

    pruebas

--- proyectoWeb/settings.py
+++ proyectoWeb/settings.py
@@ -12,6 +12,7 @@ En settings
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+EMAIL_HOST_USER="nicolas.programador@gmail.com"
+EMAIL_HOST_PASSWORD="fwwi  iqug dnqs btxu"
