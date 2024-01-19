3ca3a4536032ce91973c2318c9d017b62d190aef
zasory <nicolas.programador@gmail.com>
2024-01-19 15:21:33 -0300

    tienda terminado

--- proyectoWeb/urls.py
+++ proyectoWeb/urls.py
@@ -5,6 +5,7 @@ from django.urls import path , include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('servicios/', include('servicios.urls')),
+    path('tienda/', include('tienda.urls')),
     path('blog/', include('blog.urls')),
     path('contacto/', include('contacto.urls')),
     path('' , include('ProyectoWebApp.urls')),
